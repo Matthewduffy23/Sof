cd49fb55511a7fcecd4bcabe406cde476c544c4d
Matthewduffy23 <matthewduffy2000@gmail.com>
2025-10-07 13:46:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -812,7 +812,7 @@ for idx,row in ranked.iterrows():
             "</div>"
         )
         st.markdown(col_html, unsafe_allow_html=True)
-p completions per 90"),
+            ("Deep completions per 90"),
             ("Dribbles","Dribbles per 90"),
             ("Dribbling Success %","Successful dribbles, %"),
             ("Key Passes","Key passes per 90"),
